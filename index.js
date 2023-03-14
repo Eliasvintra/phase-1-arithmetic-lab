5562f255d7b0c594e9d6acc8ae903d414c2af943
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-14 21:44:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const multiply = 62;
+let num1 = 31;
+let num2 = 2
+
+num1 * num2; 
+
+const random = 1;
+Math.ceil(Math.random());
+
+const mod = 4;
+let num3 = 13;
+let num4 = 3;
+
+num3 % num4;
+
+const max = 20;
+Math.max(15,16,17,18,19,20);
+
+
 
 // Write your code here
